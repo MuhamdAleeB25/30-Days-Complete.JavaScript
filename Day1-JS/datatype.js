fb4edfe7683caeaa1cead18178440f8ff2000c14
MuhamdAleeB25 <b.muhamadalee@gmail.com>
2026-05-01 16:26:08 +0500

    Final fix: files moved to Day1-JS folder

---
+++ Day1-JS/datatype.js
@@ -0,0 +1,11 @@
+//JavaScript is a dynamically type language
+
+
+let age = 25;     //numbers
+let message = "Be Strong Always!!";   //string
+let islearning = true;   //boolean
+let emptyvalue = null;   //object (js old buggs happen there.)
+let undefinedvalue;     //undefine
+let percentage = 9.99;  //you represent percentages using normal number values.
+
+console.log(typeof percentage);    //for checking the values of  types
