087e3a2011fc4048c21f26b2cb502d1d74f5f203
MuhamdAleeB25 <b.muhamadalee@gmail.com>
2026-05-03 05:09:53 +0500

    fix: proper folder naming sequence

--- Day1-JS/variable.js
+++
@@ -1,9 +0,0 @@
-// In Javascript there are 3 methods of storing in Variables:
-// var let const
-
-let username = "Johnn";
-const gravity =  3.14;
-
-console.log("name" , username);   //let can be change whenever want too..
-console.log("Gravity" , gravity);    //const  value is always permenant
-
