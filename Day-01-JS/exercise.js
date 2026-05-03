dd781a41fc479f0fbad1f323eb28b6990aa58596
MuhamdAleeB25 <b.muhamadalee@gmail.com>
2026-05-03 06:38:27 +0500

    fix: removed nested repo, restored Day-01 files

---
+++ Day-01-JS/exercise.js
@@ -0,0 +1,9 @@
+// --- EXERCISES ---
+// 1. Create an array of your friends' names (next topic snack pack).
+// 2. Create a const where to put the value of Pi .
+// 3. Concatenate (join) the two strings.
+
+let firstName = "NAS";
+let lastName = "CodeVerse";
+let fullName = '' ;
+console.log("Full Name Exercise:", fullName);
---
+++ Day-01-JS/variable.js
@@ -0,0 +1,9 @@
+// In Javascript there are 3 methods of storing in Variables:
+// var let const
+
+let username = "Johnn";
+const gravity =  3.14;
+
+console.log("name" , username);   //let can be change whenever want too..
+console.log("Gravity" , gravity);    //const  value is always permenant
+
